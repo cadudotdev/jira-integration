de5a5f8b45082174a5e112f8ea94d4d11117a88c
carlos.santos <carlos.santos@neomind.com.br>
2024-12-08 03:37:24 -0300

    add: nest, react and setup build

---
+++ apps/api/webpack.config.js
@@ -0,0 +1,26 @@
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { join } = require('path');
+
+module.exports = {
+  output: {
+    path: join(__dirname, '../../dist/apps/api'),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: true,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: 'public' },
+      ],
+    }),
+  ],
+};
---
+++ apps/web/src/app/app.tsx
@@ -0,0 +1,7 @@
+export default function App(): React.ReactNode {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div>{'Hello world!!!'}</div>
+    </div>
+  );
+}
