d22d6fbaee52ca684108c71d76ca348902bbe52e
carlos.santos <carlos.santos@neomind.com.br>
2024-12-14 19:06:58 -0300

    fix: remove public folder

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,6 +1,14 @@
 export default function App(): React.ReactNode {
   return (
     <div className="w-full h-full flex justify-center items-center">
+      <input
+        type="date"
+        name="timesheet-date"
+        id="timesheet-date"
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+      />
       <div>{'Hello world!!!'}</div>
     </div>
   );
